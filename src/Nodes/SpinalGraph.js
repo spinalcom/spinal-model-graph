5d021272201407ef17c4ddd03358abeb250911bb
Loris Ciprin <loris@ciprin.fr>
2018-10-26 15:35:42 +0200

    "add function get context"

--- src/Nodes/SpinalGraph.js
+++ src/Nodes/SpinalGraph.js
@@ -3,7 +3,7 @@ import "spinal-core-connectorjs";
 import {
     SPINAL_RELATION_LST_PTR_TYPE, SPINAL_RELATION_TYPE,
 } from "../Relations/SpinalRelationFactory"
-import { guid} from "../Utilities";
+import {guid} from "../Utilities";
 import SpinalContext from "./SpinalContext";
 
 const globalType = typeof window === "undefined" ? global : window;
@@ -11,6 +11,11 @@ const HAS_CONTEXT_RELATION_NAME = "hasContext";
 
 
 class SpinalGraph extends SpinalNode {
+    /**
+     *
+     * @param type {string}  default "SpinalGraph"
+     * @param element {Model}
+     */
     constructor(type = "SpinalGraph", element = new globalType.Model) {
         super(type, element);
         this.add_attr({
@@ -27,9 +32,23 @@ class SpinalGraph extends SpinalNode {
      */
     addContext(context) {
         if (context instanceof SpinalContext)
-            this._addToRelation(context, HAS_CONTEXT_RELATION_NAME, SPINAL_RELATION_TYPE);
+            this.addChild(context, HAS_CONTEXT_RELATION_NAME, SPINAL_RELATION_TYPE);
         else
             throw new Error("Cannot add an element which is not a context");
+    }
+
+
+    async getContext(name) {
+        let children = await this.getChildren([HAS_CONTEXT_RELATION_NAME]);
+        let res;
+
+        for (let i = 0; i < children.length; i++) {
+
+            if (children[i].info.name === name)
+                return children[i];
+        }
+
+        return "undefined";
     }
 
 }
