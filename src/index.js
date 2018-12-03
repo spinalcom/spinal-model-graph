9d7bbb54401297d2968e1ebeb4beee5da97b76ab
alexis <alexis.auriac@epitech.eu>
2018-12-03 11:40:57 +0100

    Deleted graph function lib

--- src/index.js
+++ src/index.js
@@ -28,7 +28,6 @@ import SpinalContext from "./Nodes/SpinalContext";
 import SpinalRelationRef from "./Relations/SpinalRelationRef";
 import SpinalRelationLstPtr from "./Relations/SpinalRelationLstPtr";
 import SpinalRelationPtrLst from "./Relations/SpinalRelationPtrLst";
-import * as GraphFunction from "./GraphFunctionsLib/GraphFunctions";
 import {
   SpinalRelationFactory,
   SPINAL_RELATION_TYPE,
@@ -46,6 +45,5 @@ export {
   SpinalRelationFactory,
   SPINAL_RELATION_TYPE,
   SPINAL_RELATION_LST_PTR_TYPE,
-  SPINAL_RELATION_PTR_LST_TYPE,
-  GraphFunction
+  SPINAL_RELATION_PTR_LST_TYPE
 };
