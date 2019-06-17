43259fe436b16cdd540b46a5fe145fad1c4b5532
LorisC <loris@ciprin.fr>
2019-06-17 14:24:59 +0200

    merge pbr and typescript

--- build/Utilities.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Creates a unique id based on a name.
- * @param {string} name Name from wich the id is generated
- * @returns {string} Generated id
- */
-declare function guid(name: string): string;
-export { guid, };
--- build/test/SanityCheck.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-describe('Sanity check', () => {
-    it('should return true', () => {
-        assert.equal(0, 0);
-    });
-});
-//# sourceMappingURL=SanityCheck.js.map
